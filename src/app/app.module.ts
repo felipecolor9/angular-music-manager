0f98a0e1b1921b4e00974dd1ee4824db463daec6
Felipe Marques Silva <felipe.m199@gmail.com>
2021-07-14 11:39:24 -0300

    FormsModule and MusicListComponent added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MusicListComponent } from './musics/music-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicListComponent
   ],
   imports: [
     BrowserModule,
-    NgModel
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
