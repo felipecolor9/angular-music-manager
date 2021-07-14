a2b7482a8191dda4a511b96e0f02ec6180f78b3f
Felipe Marques Silva <felipe.m199@gmail.com>
2021-07-14 18:37:18 -0300

    Music list component updated with table and mock data

--- src/app/musics/music-list.component.ts
+++ src/app/musics/music-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Music } from "./music";
 
 @Component({
     selector: 'app-music-list',
     templateUrl: './music-list.component.html'
 })
-export class MusicListComponent {
-
+export class MusicListComponent implements OnInit {
     
+    musics: Music[] = [];
+    
+    ngOnInit(): void {
+        this.musics = [ // Mock music
+            {
+                id: 1,
+                name: 'Rose in Harlem',
+                coverUrl:'',
+                artist: 'Teyana Taylor',
+                durationMS: 34200,
+                rating: 9.1,
+                releaseDate: '29/08/1998'
+            } , {
+                id: 2,
+                name: 'N.Y State of mind',
+                coverUrl:'',
+                artist: 'NAS',
+                durationMS: 45400,
+                rating: 9.8,
+                releaseDate: '20/05/1994'
+            }
+        ]
+    }
 }
