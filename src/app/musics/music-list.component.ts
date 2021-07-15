354945b101de52ce4bc7d31e969d65338365cd8b
Felipe Marques Silva <felipe.m199@gmail.com>
2021-07-15 18:10:21 -0300

    Filter method according to name with databinding created

--- src/app/musics/music-list.component.ts
+++ src/app/musics/music-list.component.ts
@@ -1,3 +1,4 @@
+import { stringify } from "@angular/compiler/src/util";
 import { Component, OnInit } from "@angular/core";
 import { Music } from "./music";
 import { MusicService } from "./music.service";
@@ -8,11 +9,23 @@ import { MusicService } from "./music.service";
 })
 export class MusicListComponent implements OnInit {
     
-    musics: Music[] = [];
+    _musics: Music[] = [];
+    _filterBy: string;
+    filteredMusics: Music[] = [];
     
     constructor(private musicService:MusicService) {}
 
     ngOnInit(): void {
-        this.musics = this.musicService.getAll();
+        this._musics = this.musicService.getAll();
+        this.filteredMusics = this._musics;
+    }
+
+    set filter(value : string) {
+        this._filterBy = value;
+        this.filteredMusics = this._musics.filter((music: Music) => music.name.toLowerCase().indexOf(this._filterBy.toLowerCase()) > -1);
+    }
+
+    get filter() {
+        return this._filterBy;
     }
 }
