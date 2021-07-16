987a03d9284e3bab224c464abb75b2de91c946cf
Felipe Marques Silva <felipe.m199@gmail.com>
2021-07-16 15:34:08 -0300

    Music info component initial form finished

--- src/app/musics/music-info.component.ts
+++ src/app/musics/music-info.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Music } from "./music";
+import { MusicService } from "./music.service";
 
 @Component ({
     templateUrl: './music-info.component.html'
 })
 export class MusicInfoComponent implements OnInit {
 
-    musicId: number;
+    music: Music;
 
-    constructor(private activatedRoute: ActivatedRoute) { } // Active Route
+    constructor(private activatedRoute: ActivatedRoute, private musicService: MusicService) { } // Active Route
 
     ngOnInit(): void {
-       this.musicId = +this.activatedRoute.snapshot.paramMap.get('id');
+       this.music = this.musicService.retrieveByID(+this.activatedRoute.snapshot.paramMap.get('id'));
     }
 
 }
