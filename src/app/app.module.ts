08422203118de97dedde47cc359397ca4551be6b
Felipe Marques Silva <felipe.m199@gmail.com>
2021-07-15 22:03:13 -0300

    RouterModule, Error404 and NavigationBar imported and implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { StarComponent } from './star/star.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error-404/error-404.component';
 
 import { AppComponent } from './app.component';
 import { MusicListComponent } from './musics/music-list.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavigationBar } from './nav_bar/nav_bar.compoment';
 
 @NgModule({
   declarations: [
     AppComponent,
     MusicListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavigationBar,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'', redirectTo:'musics', pathMatch: 'full'
+      },
+      {
+        path:'musics', component: MusicListComponent  
+      },
+      {
+        path:'**', component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
