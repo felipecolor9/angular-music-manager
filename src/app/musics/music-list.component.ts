0f7b7b6b7ccdc02c57ceabaf8f4a33b4dd599524
Felipe Marques Silva <felipe.m199@gmail.com>
2021-07-17 22:02:10 -0300

    Methods 'retrieveAll' and 'deleteById' via HTTP request added

--- src/app/musics/music-list.component.ts
+++ src/app/musics/music-list.component.ts
@@ -13,10 +13,25 @@ export class MusicListComponent implements OnInit {
     
     constructor(private musicService:MusicService) {}
 
-    ngOnInit(): void {
-        this._musics = this.musicService.getAll();
-        this.filteredMusics = this._musics;
+    ngOnInit(): void {this.retrieveAll()}
+
+    retrieveAll():void {
+        this.musicService.getAll().subscribe({
+            next: musics => { this._musics = musics;
+            this.filteredMusics = this._musics;
+            },
+            error: err => console.log('Error', err)
+        });  
     }
+
+    deleteById(id: number): void {
+        this.musicService.deleteById(id).subscribe({
+            next: () => { console.log('Deleted with success!');
+            this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        })
+    } 
 
     set filter(value : string) {
         this._filterBy = value;
